79b84ab2adbb0d61d110ebcefc3069e75c25d00d
Abraham-K-C <ccasaabraham02@gmail.com>
2026-06-30 10:38:01 -0500

    upload: microservicios/catalogo-service/src/main/java/com/gamesrentall/catalogo/repository/JuegoRepository.java

---
+++ microservicios/catalogo-service/src/main/java/com/gamesrentall/catalogo/repository/JuegoRepository.java
@@ -0,0 +1,9 @@
+package com.gamesrentall.catalogo.repository;
+
+import com.gamesrentall.catalogo.entity.Juego;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JuegoRepository extends JpaRepository<Juego, Long> {
+}
