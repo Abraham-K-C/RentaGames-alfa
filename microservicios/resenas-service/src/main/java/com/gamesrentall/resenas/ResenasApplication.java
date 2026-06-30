348951f7eea8397ececa1a9223d01ff065e7e96f
Abraham-K-C <ccasaabraham02@gmail.com>
2026-06-30 10:39:05 -0500

    upload: microservicios/resenas-service/src/main/java/com/gamesrentall/resenas/ResenasApplication.java

---
+++ microservicios/resenas-service/src/main/java/com/gamesrentall/resenas/ResenasApplication.java
@@ -0,0 +1,13 @@
+package com.gamesrentall.resenas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ResenasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ResenasApplication.class, args);
+    }
+}
